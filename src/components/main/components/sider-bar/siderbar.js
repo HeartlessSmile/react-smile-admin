02f50cba8f4511112e56638f90dea318567f77da
smile <penggang@danyuantech.com>
2019-04-10 14:48:06 +0800

    侧边动态渲染

--- src/components/main/components/sider-bar/siderbar.js
+++ src/components/main/components/sider-bar/siderbar.js
@@ -2,24 +2,49 @@ import React, {
     Component
 } from 'react';
 import { Menu, Icon } from 'antd';
+import {Link,withRouter} from 'react-router-dom'
 import './siderbar.less'
 class SiderBar extends Component {
+    state={
+
+    }
+    renderSubMenu = ({key,icon,title,children})=>{
+        return (
+            <Menu.SubMenu key={key} title={<span>{icon && <Icon type={icon}/>}<span>{title}</span></span>}>
+                {
+                  children && children.map(item => {
+                    return item.children && item.children.length > 0 ? this.renderSubMenu(item) : this.renderMenuItem(item)
+                  })
+                }
+            </Menu.SubMenu>
+        )
+    }
+    renderMenuItem = ({key,icon,title})=>{
+        // return (
+        //   <Menu.Item key={key}>
+        //     <Link to={key}>
+        //       {icon && <Icon type={icon}/>}
+        //       <span>{title}</span>
+        //     </Link>
+        //   </Menu.Item>
+        // )
+
+        return (
+            <Menu.Item key={key}>
+              {icon && <Icon type={icon}/>}
+              <span>{title}</span>
+            </Menu.Item>
+        )
+    }
     render() {
         return (
             <div className="layout_sider_box">
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-		            <Menu.Item key="1">
-		              <Icon type="user" />
-		              <span>nav 1</span>
-		            </Menu.Item>
-		            <Menu.Item key="2">
-		              <Icon type="video-camera" />
-		              <span>nav 2</span>
-		            </Menu.Item>
-		            <Menu.Item key="3">
-		              <Icon type="upload" />
-		              <span>nav 3</span>
-		            </Menu.Item>
+                {
+                  this.props.menus && this.props.menus.map(item => {
+                    return item.children && item.children.length > 0 ? this.renderSubMenu(item) : this.renderMenuItem(item)
+                  })
+                }
 		         </Menu>
             </div>
         )
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -6,6 +6,28 @@ import HeaderBar from './components/header-bar/headerbar.js';
 import SiderBar from './components/sider-bar/siderbar.js';
 import "./index.less";
 const { Content, Sider } = Layout;
+const menus = [
+    {
+        title:"首页",
+        icon:"home",
+        key:"/home"
+    },
+    {
+        title:"配置中心",
+        icon:"desktop",
+        key:"/configure",
+        children:[
+            {
+                title:"资源管理",
+                key:"/configure/resources"
+            },
+            {
+                title:"权限管理",
+                key:"/configure/jurisdiction"
+            }
+        ]
+    }
+]
 class Main extends Component {
     state={
         collapsed:false
@@ -19,12 +41,12 @@ class Main extends Component {
         const collapsed = this.state.collapsed
         return (
             <Layout className="main_box">
-                <Sider width={256} style={{ background: '#14192A' }} collapsed={this.state.collapsed}>
+                <Sider width={256} style={{ background: '#001529' }} collapsed={this.state.collapsed}>
                     <div className="logo">
                         <img src={require('@/assets/image/logo.png')} alt="" />
                         {collapsed ?'':(<span className="logoWord">XXX后台系统</span>)}
                     </div>
-                    <SiderBar></SiderBar>
+                    <SiderBar menus={menus}></SiderBar>
                 </Sider>
                 <Layout>
                     <HeaderBar collapsed={this.state.collapsed} collapsedClick={this.onCollapse.bind(this)}></HeaderBar>
